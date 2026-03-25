8d06e1300eb04dcae70972a0d17d4678f2327ce0
Shayan Halder <shayanhalder2389@gmail.com>
2026-03-25 14:13:07 -0700

    update deploy-frontend.sh

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -4,11 +4,13 @@ directory="$HOME/app/frontend"
 cd $directory
 echo "Building static production files..."
 npm run build
-echo "Copying build files into /var/www/hayeslab/..."
-sudo cp -r dist/* /var/www/hayeslab/
+echo "Copying build files into /var/www/SANA_Website/..."
+mkdir -p /var/www/SANA_Website/
+sudo cp -r dist/* /var/www/SANA_Website/
 echo "Adding read permissions..."
-sudo chmod -R 755 /var/www/hayeslab
-sudo ln -s /etc/nginx/sites-available/hayeslab /etc/nginx/sites-enabled/
+sudo chmod -R 755 /var/www/SANA_Website/
+mkdir -p /etc/nginx/sites-available/SANA_Website/
+sudo ln -s /etc/nginx/sites-available/SANA_Website /etc/nginx/sites-enabled/
 sudo nginx -t
 echo "Restarting nginx server..."
 sudo systemctl reload nginx
